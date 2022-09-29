c65f711c71bc6c9f2a9df16e1cf33f2e091a16bc
lining1111 <770677655@qq.com>
2022-09-29 18:41:08 +0800

    增加libxml2库

---
+++ gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/aarch64-linux-gnu/libc/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/usr/local/arm64/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/aarch64-linux-gnu/libc/usr/lib"
+XML2_LIBS="-lxml2 -lz   -liconv -lm "
+XML2_INCLUDEDIR="-I/usr/local/arm64/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/aarch64-linux-gnu/libc/usr/include/libxml2"
+MODULE_VERSION="xml2-2.9.0"
+
